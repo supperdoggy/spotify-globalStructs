968f68e2717b4424813b405b13a27efbe87ecf2d
supperdoggy <supperspggy229@gmail.com>
2021-11-25 00:18:15 +0200

    song update

--- song.go
+++ song.go
@@ -9,3 +9,8 @@ type Song struct {
 	ReleaseDate time.Time `json:"release_date"`
 	Path        string    `json:"path"`
 }
+
+type SongData struct {
+	ID string `json:"id" mgo:"_id"`
+	Data []byte `json:"data" mgo:"data"`
+}
